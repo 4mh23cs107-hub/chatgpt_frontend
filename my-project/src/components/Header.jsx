c9a7458f98b78c4345fd68848bdcea3d68915da7
4mh23cs107 <4mh23cs107@gmail.com>
2026-02-12 14:19:01 +0530

    front

--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+
 const Header = () => {
+    const location = useLocation();
+    const isDashboard = location.pathname === '/dashboard';
+
     return (
         <div>
             <nav style={{ padding: '1rem', backgroundColor: '#333', marginBottom: '1rem' }}>
-                <Link to="/" style={{ color: 'white', marginRight: '1rem' }}>Home</Link>
-                <Link to="/about" style={{ color: 'white', marginRight: '1rem' }}>About</Link>
-                <Link to="/contact" style={{ color: 'white', marginRight: '1rem' }}>Contact</Link>
-                <Link to="/login" style={{ color: 'white', marginRight: '1rem' }}>Login</Link>
-                <Link to="/signup" style={{ color: 'white', marginRight: '1rem' }}>Signup</Link>
+                {!isDashboard && (
+                    <>
+                        <Link to="/" style={{ color: 'white', marginRight: '1rem' }}>Home</Link>
+                        <Link to="/about" style={{ color: 'white', marginRight: '1rem' }}>About</Link>
+                        <Link to="/contact" style={{ color: 'white', marginRight: '1rem' }}>Contact</Link>
+                        <Link to="/login" style={{ color: 'white', marginRight: '1rem' }}>Login</Link>
+                        <Link to="/signup" style={{ color: 'white', marginRight: '1rem' }}>Signup</Link>
+                    </>
+                )}
             </nav>
         </div>
     )
