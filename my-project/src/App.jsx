06ad9b37b73e30d8dc892258022177759316792e
4mh23cs107 <4mh23cs107@gmail.com>
2026-02-12 11:32:56 +0530

    front

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
-<<<<<<< HEAD
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
@@ -27,35 +25,6 @@ function App() {
       </Routes>
       <Footer />
     </Router>
-=======
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
->>>>>>> d8213194ab3035e8063da74c39ae0be3fbf72d53
   )
 }
 
